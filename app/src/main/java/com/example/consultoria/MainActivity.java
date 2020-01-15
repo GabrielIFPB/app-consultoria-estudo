c2dc92456ab65818bcf0cd315296dfee2c17216b
gabriel <jeronimogbariel@gmail.com>
2020-01-15 11:24:09 -0300

    criado activity serviço e inicializando activity cliente

--- app/src/main/java/com/example/consultoria/MainActivity.java
+++ app/src/main/java/com/example/consultoria/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.consultoria;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
+
+    private ImageView btnSobreEmpresa;
+    private ImageView btnServico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +18,22 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
+
+        this.btnSobreEmpresa = findViewById(R.id.id_btn_empresa);
+        this.btnServico = findViewById(R.id.id_btn_servico);
+
+        this.btnSobreEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SobreEmpresaActivity.class));
+            }
+        });
+
+        this.btnServico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ServicoActivity.class));
+            }
+        });
     }
 }
