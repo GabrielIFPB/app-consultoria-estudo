573c5c66e17bb0c1ea10e752c59ef680c2cfc95f
gabriel <jeronimogbariel@gmail.com>
2020-01-15 19:23:18 -0300

    activity cliente ok

--- app/src/main/java/com/example/consultoria/MainActivity.java
+++ app/src/main/java/com/example/consultoria/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
 
     private ImageView btnSobreEmpresa;
     private ImageView btnServico;
+    private ImageView btnCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,7 @@ public class MainActivity extends Activity {
 
         this.btnSobreEmpresa = findViewById(R.id.id_btn_empresa);
         this.btnServico = findViewById(R.id.id_btn_servico);
+        this.btnCliente = findViewById(R.id.id_btn_clientes);
 
         this.btnSobreEmpresa.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +35,13 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, ServicoActivity.class));
+            }
+        });
+
+        this.btnCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ClienteActivity.class));
             }
         });
     }
